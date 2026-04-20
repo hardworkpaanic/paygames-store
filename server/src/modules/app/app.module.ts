91281809ad2d6df2dd29cc30d41c2926978c8952
hardworkpaanic <faktorialandrej@gmail.com>
2026-04-20 18:39:03 +0400

    init all modules

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from '../users/users.module';
+import { ProductsModule } from '../products/products.module';
+import { OrdersModule } from '../orders/orders.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    ProductsModule,
+    OrdersModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
